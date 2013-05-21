df307ef0ba17de8e8f8fb0e3c75ef23a66e364d7
Daniel Kennedy <danwkennedy@gmail.com>
2013-05-20 22:01:19 -0400

    Added basic navigation.
    The navigation items don't go anywhere or accomplish any actions but
    it's a start :)

--- Android/Schedulife/src/com/lmaas/schedulife/MainActivity.java
+++ Android/Schedulife/src/com/lmaas/schedulife/MainActivity.java
@@ -3,45 +3,75 @@ package com.lmaas.schedulife;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
-@SuppressLint("NewApi")
 public class MainActivity extends SherlockActivity implements OnNavigationListener {
 
-	private String[] mLocations;
+	String[] mScheduleViews;
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_main);
-        
-        mLocations = getResources().getStringArray(R.array.locations);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mScheduleViews = getResources().getStringArray(R.array.scheduleViews);
         
         Context context = getSupportActionBar().getThemedContext();
-        ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(context, R.array.locations, R.layout.sherlock_spinner_item);
+        ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(context, R.array.scheduleViews, R.layout.sherlock_spinner_item);
         list.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
         
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-        getSupportActionBar().setListNavigationCallbacks(list, (OnNavigationListener) this);
-    }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        actionBar.setListNavigationCallbacks(list, (OnNavigationListener) this);
+        actionBar.setDisplayShowTitleEnabled(false);
+	}
 
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-    
     @Override
-    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-    	System.out.println("Slected: " + mLocations[itemPosition]);
-    	return true;
-    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+    	MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main,  menu);
+        return true;
+    }   
+	
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		
+		switch(itemPosition) {
+		case 0:
+			break;
+		case 1:
+			break;
+		}
+		
+		System.out.println("Changing view to: " + mScheduleViews[itemPosition]);
+		
+		return false;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch(item.getItemId()) {
+		case R.id.action_prioritize:
+			System.out.println("Opening prioritize modal");
+			break;
+		case R.id.action_addTask:
+			System.out.println("Opening add task form");
+			break;
+		case R.id.action_showCategories:
+			System.out.println("Navigating to the Categories activity");
+			break;
+		case R.id.action_settings:
+			System.out.println("Navigating to the Settings activity");
+		}		
+		
+		return false;
+	}
+	
     
 }
